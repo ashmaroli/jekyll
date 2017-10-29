37f329e906269e228853029f40074b62693c387a
Jordon Bedwell <jordon@envygeeks.io>
2017-10-29 09:43:09 -0500

    Fix #6498: Use Gem to discover the location of bundler.

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -143,7 +143,8 @@ RUBY
         def bundle_install(path)
           Jekyll.logger.info "Running bundle install in #{path.cyan}..."
           Dir.chdir(path) do
-            process, output = Jekyll::Utils::Exec.run("bundle", "install")
+            exe = Gem.bin_path("bundler", "bundle")
+            process, output = Jekyll::Utils::Exec.run(exe, "install")
             output.to_s.each_line do |line|
               Jekyll.logger.info("Bundler:".green, line.strip) unless line.to_s.empty?
             end
