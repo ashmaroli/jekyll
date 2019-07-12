1a7a7fab0c625fbb600f5918c0b44c35547a7658
Ashwin Maroli <ashmaroli@gmail.com>
2019-07-12 14:24:40 +0530

    Remove experimental path_manager changes

--- lib/jekyll.rb
+++ lib/jekyll.rb
@@ -171,7 +171,22 @@ module Jekyll
     def sanitized_path(base_directory, questionable_path)
       return base_directory if base_directory.eql?(questionable_path)
 
-      PathManager.sanitized_join(base_directory, questionable_path)
+      clean_path = questionable_path.dup
+      clean_path.insert(0, "/") if clean_path.start_with?("~")
+      clean_path = File.expand_path(clean_path, "/")
+
+      return clean_path if clean_path.eql?(base_directory)
+
+      # remove any remaining extra leading slashes not stripped away by calling
+      # `File.expand_path` above.
+      clean_path.squeeze!("/")
+
+      if clean_path.start_with?(base_directory.sub(%r!\z!, "/"))
+        clean_path
+      else
+        clean_path.sub!(%r!\A\w:/!, "/")
+        File.join(base_directory, clean_path)
+      end
     end
 
     # Conditional optimizations
--- lib/jekyll/path_manager.rb
+++ lib/jekyll/path_manager.rb
@@ -8,31 +8,6 @@ module Jekyll
         @join[base] ||= {}
         @join[base][item] ||= File.join(base, item).freeze
       end
-
-      def sanitized_join(base, input)
-        @sanitized_join ||= {}
-        @sanitized_join[base] ||= {}
-        @sanitized_join[base][input] ||= begin
-          input = input.dup
-          input.insert(0, "/") if input.start_with?("~")
-          input = File.expand_path(input, "/")
-
-          if input.eql?(base)
-            input
-          else
-            # remove any remaining extra leading slashes not stripped away by calling
-            # `File.expand_path` above.
-            input.squeeze!("/")
-
-            if input.start_with?("#{base}/")
-              input
-            else
-              input.sub!(%r!\A\w:/!, "/")
-              join(base, input).freeze
-            end
-          end
-        end
-      end
     end
   end
 end
