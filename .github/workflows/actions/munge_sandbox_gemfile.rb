31c81acb97e4de1ac306b8f693a872116011f8fb
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-16 18:23:24 +0530

    Fix gemfile_munger script

--- .github/workflows/actions/munge_sandbox_gemfile.rb
+++ .github/workflows/actions/munge_sandbox_gemfile.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 SANDBOX_GEMFILE = File.expand_path('../../../../sandbox/Gemfile', __dir__)
-contents = File.read(SANDBOX_GEMFILE).gsub(%r!gem 'jekyll'(.*)!, "gem 'jekyll', path: '../jekyll'")
-File.open(SANDBOX_GEMFILE, 'wb') { |f| f.puts(contents) }
+CONTENTS = <<~RUBY
+  source 'https://rubygems.org'
+
+  gem 'jekyll', path: '../jekyll'
+
+  group :jekyll_plugins do
+    gem 'jekyll-mentions'
+    gem 'jekyll-paginate'
+    gem 'jekyll-redirect-from'
+    gem 'jekyll-seo-tag'
+    gem 'jekyll-seo'
+    gem 'jekyll-sitemap'
+  end
+RUBY
+
+File.open(SANDBOX_GEMFILE, 'wb') { |f| f.puts(CONTENTS) }
