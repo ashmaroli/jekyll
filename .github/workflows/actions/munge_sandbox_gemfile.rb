993b79480e58159902aa76be58cb68e38a1a5112
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-16 17:50:04 +0530

    Add workflow to build third-party repo with Jekyll

---
+++ .github/workflows/actions/munge_sandbox_gemfile.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+SANDBOX_GEMFILE = 'sandbox/Gemfile'
+contents = File.read(SANDBOX_GEMFILE).gsub(%r!gem 'jekyll'(.*)!, "gem 'jekyll', path: '../jekyll'")
+File.open(SANDBOX_GEMFILE, 'wb') { |f| f.puts(contents) }
