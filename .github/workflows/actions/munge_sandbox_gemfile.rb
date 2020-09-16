ec0b31e58b729879262b4a8c216a4e312f3d0cf1
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-16 18:03:53 +0530

    Fix ruby script invocation path

--- .github/workflows/actions/munge_sandbox_gemfile.rb
+++ .github/workflows/actions/munge_sandbox_gemfile.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-SANDBOX_GEMFILE = 'sandbox/Gemfile'
+SANDBOX_GEMFILE = File.expand_path('../../../../sandbox/Gemfile', __dir__)
 contents = File.read(SANDBOX_GEMFILE).gsub(%r!gem 'jekyll'(.*)!, "gem 'jekyll', path: '../jekyll'")
 File.open(SANDBOX_GEMFILE, 'wb') { |f| f.puts(contents) }
