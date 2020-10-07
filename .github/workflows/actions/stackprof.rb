6398ee0e49a138f6f67e42f30c67acbe3369abb0
Ashwin Maroli <ashmaroli@gmail.com>
2020-10-07 20:30:09 +0530

    Profile call stack of Jekyll Build

---
+++ .github/workflows/actions/stackprof.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "jekyll"
+require "stackprof"
+
+PROF_OUTPUT_FILE = File.expand_path(".stackprof-cpu.dump", __dir__).freeze
+
+StackProf.run(mode: :cpu, out: PROF_OUTPUT_FILE) do
+  Jekyll::PluginManager.require_from_bundler
+  Jekyll::Commands::Build.process({
+    "source"             => File.expand_path(ARGV[0]),
+    "destination"        => File.expand_path("#{ARGV[0]}/_site"),
+    "disable_disk_cache" => true,
+  })
+  puts ""
+end
+
+StackProf::Report.new(Marshal.load(IO.binread(PROF_OUTPUT_FILE))).print_text
