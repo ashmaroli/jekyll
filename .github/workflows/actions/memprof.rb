1a094b20f3351ac51ffdcdf78ed24cc254783876
Ashwin Maroli <ashmaroli@gmail.com>
2020-11-02 20:50:33 +0530

    Trace Array allocations from Jekyll

--- .github/workflows/actions/memprof.rb
+++ .github/workflows/actions/memprof.rb
@@ -3,7 +3,7 @@
 require 'jekyll'
 require 'memory_profiler'
 
-MemoryProfiler.report(allow_files: 'kramdown') do
+MemoryProfiler.report(trace: [Array], allow_files: 'lib/jekyll') do
   Jekyll::PluginManager.require_from_bundler
   Jekyll::Commands::Build.process({
     "source"             => File.expand_path(ARGV[0]),
