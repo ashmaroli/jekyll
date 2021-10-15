6b563be0ca5482d67a9f4463b3ae3c7c296a6161
Ashwin Maroli <ashmaroli@gmail.com>
2021-10-15 15:25:49 +0530

    Refactor benchmark script

--- benchmark/include_tag.rb
+++ benchmark/include_tag.rb
@@ -20,13 +20,11 @@ class Base
   PARAMS_PATTERN = Jekyll::Tags::IncludeTag::VALID_SYNTAX
   VARIABLE_FILE  = Jekyll::Tags::IncludeTag::VARIABLE_SYNTAX
 
-  def render
-    10000.times do
-      extract_params_hash
+  def render(context)
+    1000.times do
+      extract_params_hash(context)
     end
   end
-
-  def extract_params_hash; end
 end
 
 class LegacyInclude < Base
@@ -40,7 +38,7 @@ class LegacyInclude < Base
     end
   end
 
-  def extract_params_hash
+  def extract_params_hash(context)
     params = {}
     markup = @params
 
@@ -52,7 +50,7 @@ class LegacyInclude < Base
               elsif match[3]
                 match[3].gsub("\\'", "'")
               elsif match[4]
-                CONTEXT[match[4]]
+                context[match[4]]
               end
 
       params[match[1]] = value
@@ -73,7 +71,7 @@ class OptimizedInclude < Base
     end
   end
 
-  def extract_params_hash
+  def extract_params_hash(context)
     params = {}
     @params.scan(PARAMS_PATTERN) do |key, d_quoted, s_quoted, variable|
       value = if d_quoted
@@ -81,7 +79,7 @@ class OptimizedInclude < Base
               elsif s_quoted
                 s_quoted.include?("\\'") ? s_quoted.gsub("\\'", "'") : s_quoted
               elsif variable
-                CONTEXT[variable]
+                context[variable]
               end
 
       params[key] = value
@@ -91,12 +89,42 @@ class OptimizedInclude < Base
 end
 
 class SuperOptimizedInclude < Base
+  class ParameterTokenList
+    def initialize(list)
+      @list = list.map! do |key, d_quoted, s_quoted, variable|
+        [
+          key,
+          d_quoted&.include?('\\"') ? d_quoted.gsub('\\"', '"') : d_quoted,
+          s_quoted&.include?("\\'") ? s_quoted.gsub("\\'", "'") : s_quoted,
+          variable,
+        ]
+      end
+      @variable = @list.any? { |token| token[3] }
+    end
+
+    def render(context)
+      @variable ? variable_hash(context) : static_hash
+    end
+
+    def static_hash
+      @static_hash ||= @list.each_with_object({}) do |(key, d_quoted, s_quoted, _), params|
+        params[key] = d_quoted || s_quoted
+      end
+    end
+
+    def variable_hash(context)
+      @list.each_with_object({}) do |(key, d_quoted, s_quoted, variable), params|
+        params[key] = d_quoted || s_quoted || context[variable]
+      end
+    end
+  end
+
   def initialize(markup)
     markup  = markup.strip
     matched = markup.match(VARIABLE_FILE)
     if matched
-      @file = matched["variable"].strip
-      @params = matched["params"].strip
+      @file = matched["variable"].tap(&:strip!)
+      @params = matched["params"].tap(&:strip!)
     else
       @file, @params = markup.split(%r!\s+!, 2)
     end
@@ -107,46 +135,72 @@ class SuperOptimizedInclude < Base
   end
 
   def tokenize_params
-    @param_tokens = @params.scan(PARAMS_PATTERN).map! do |key, d_quoted, s_quoted, variable|
-      [
-        key,
-        (d_quoted&.include?('\\"') ? d_quoted.gsub('\\"', '"') : d_quoted),
-        (s_quoted&.include?("\\'") ? s_quoted.gsub("\\'", "'") : s_quoted),
-        variable,
-      ]
-    end
+    @param_tokens = ParameterTokenList.new(@params.scan(PARAMS_PATTERN))
   end
 
-  def extract_params_hash
-    params = {}
-    @param_tokens.each do |key, d_quoted, s_quoted, variable|
-      value = if d_quoted
-                d_quoted
-              elsif s_quoted
-                s_quoted
-              elsif variable
-                CONTEXT[variable]
-              end
-      params[key] = value
-    end
-    params
+  def extract_params_hash(context)
+    @param_tokens.render(context)
   end
 end
 
+#
+
 class Profiler
-  KLASSES = [LegacyInclude, OptimizedInclude, SuperOptimizedInclude]
+  KLASSES = {
+    'legacy'          => LegacyInclude,
+    'optimized'       => OptimizedInclude,
+    'super-optimized' => SuperOptimizedInclude
+  }
 
   def initialize(markup)
     @markup = markup
-    @stash  = {}
+    @stack  = []
+  end
 
-    KLASSES.each do |klass|
-      @stash[klass.name] = MemoryProfiler.report { klass.new(markup).render }
+  def test
+    puts ""
+    puts "MARKUP: #{@markup.cyan}"
+    puts ""
+    puts <<~TEXT
+      Param Hash Results #{"-" * 30}
+      #{debug}
+    TEXT
+    puts ""
+  end
+
+  def benchmark
+    Benchmark.ips do |x|
+      KLASSES.each do |label, klass|
+        x.report(label) { klass.new(@markup).render(CONTEXT) }
+      end
+      x.compare!
     end
   end
 
-  def reports
-    @stash.values.map do |reporter|
+  def profile_memory
+    KLASSES.each_value do |klass|
+      @stack << MemoryProfiler.report { klass.new(@markup).render(CONTEXT) }
+    end
+
+    puts Terminal::Table.new(
+      :title    => "MEMORY PROFILE",
+      :headings => [" "].concat(KLASSES.keys),
+      :rows     => memprof_reports
+    )
+    puts ""
+    puts "=" * 100
+  end
+
+  private
+
+  def debug
+    KLASSES.map do |label, klass|
+      [label.rjust(20), ": ", klass.new(@markup).extract_params_hash(CONTEXT)].join
+    end.join("\n")
+  end
+
+  def memprof_reports
+    @stack.map do |reporter|
       allocated_memory  = reporter.scale_bytes(reporter.total_allocated_memsize)
       allocated_objects = reporter.total_allocated
       retained_memory   = reporter.scale_bytes(reporter.total_retained_memsize)
@@ -161,29 +215,9 @@ class Profiler
 end
 
 [MARKUP_1, MARKUP_2, MARKUP_3, MARKUP_4, MARKUP_5, MARKUP_6].each do |markup|
-  puts ""
-  puts "MARKUP: #{markup.cyan}"
-  puts ""
-  puts <<~TEXT
-    Param Hash Results #{"-" * 30}
-             LEGACY: #{LegacyInclude.new(markup).extract_params_hash}
-          OPTIMIZED: #{OptimizedInclude.new(markup).extract_params_hash}
-    SUPER-OPTIMIZED: #{SuperOptimizedInclude.new(markup).extract_params_hash}
-  TEXT
-  puts ""
+  profiler = Profiler.new(markup)
 
-  Benchmark.ips do |x|
-    x.report('legacy') { LegacyInclude.new(markup).render }
-    x.report('optimized') { OptimizedInclude.new(markup).render }
-    x.report('super-optimized') { SuperOptimizedInclude.new(markup).render }
-    x.compare!
-  end
-
-  puts Terminal::Table.new(
-    :title    => "MEMORY PROFILE",
-    :headings => [" ", "LEGACY", "OPTIMIZED", "SUPER-OPTIMIZED"],
-    :rows     => Profiler.new(markup).reports
-  )
-  puts ""
-  puts "=" * 100
+  profiler.test
+  profiler.benchmark
+  profiler.profile_memory
 end
