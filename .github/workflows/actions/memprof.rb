4dd6eca168defee90ed01ee78d617d0b1f3d4712
Ashwin Maroli <ashmaroli@gmail.com>
2023-01-12 20:46:38 +0530

    Append profile summary to step_summary

--- .github/workflows/actions/memprof.rb
+++ .github/workflows/actions/memprof.rb
@@ -3,12 +3,33 @@
 require 'jekyll'
 require 'memory_profiler'
 
-MemoryProfiler.report(allow_files: ['lib/jekyll/', 'lib/jekyll.rb']) do
+reporter = MemoryProfiler.report(allow_files: ['lib/jekyll/', 'lib/jekyll.rb']) do
   Jekyll::PluginManager.require_from_bundler
   Jekyll::Commands::Build.process({
     "source"             => File.expand_path(ARGV[0]),
     "destination"        => File.expand_path("#{ARGV[0]}/_site"),
     "disable_disk_cache" => true,
+    "quiet"              => true,
   })
   puts ''
-end.pretty_print(scale_bytes: true, normalize_paths: true)
+end
+
+reporter.instance_variable_set(:@colorize, MemoryProfiler::Monochrome.new)
+
+total_allocated_output = reporter.scale_bytes(reporter.total_allocated_memsize)
+total_retained_output  = reporter.scale_bytes(reporter.total_retained_memsize)
+
+puts <<~HTML
+  <pre>
+  Total allocated: #{total_allocated_output} (#{reporter.total_allocated} objects)
+  Total retained:  #{total_retained_output} (#{reporter.total_retained} objects)
+  </pre>
+  <p>
+    <details>
+      <summary>Full Report</summary>
+      <pre>
+#{reporter.print_string_reports($stdout, {scale_bytes: true, normalize_paths: true})}
+      </pre>
+    </details>
+  </p>
+HTML
