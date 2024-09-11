cca43fe6031798c788ee3f1765f07f2df44be98a
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2024-09-11 17:23:59 +0530

    Fix `foo` command

--- lib/jekyll/commands/foo.rb
+++ lib/jekyll/commands/foo.rb
@@ -2,8 +2,8 @@
 
 module Jekyll
   module Commands
-    class Help < Command
-      def init_with_program(prog)
+    class Foo < Command
+      def self.init_with_program(prog)
         prog.command(:foo) do |c|
           c.syntax "foo [options]"
           c.option "ignore", "--ignore PAT[,PAT2[,...]]", Array, "Files to ignore"
