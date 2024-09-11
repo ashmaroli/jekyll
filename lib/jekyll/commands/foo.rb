9291b4b8b62612e21885e6136fb8855dfe30fb4d
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2024-09-11 17:18:02 +0530

    Add `foo` command

---
+++ lib/jekyll/commands/foo.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Commands
+    class Help < Command
+      def init_with_program(prog)
+        prog.command(:foo) do |c|
+          c.syntax "foo [options]"
+          c.option "ignore", "--ignore PAT[,PAT2[,...]]", Array, "Files to ignore"
+
+          c.action do |args, opts|
+            p args
+            p opts
+          end
+        end
+      end
+    end
+  end
+end
